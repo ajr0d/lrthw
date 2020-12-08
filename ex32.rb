5f970e0e719cb228e7231fa101339dd72c61686f
aj <65660516+ajr0d@users.noreply.github.com>
2020-12-08 11:48:43 +0000

    Add ex32.rb

---
+++ ex32.rb
@@ -0,0 +1,43 @@
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+# this first kind of for-loop goes through a list
+# in a more traditional style found in other languages
+#for number in the_count
+#  puts "This is count #{number}"
+#end
+
+the_count.each do |number|
+  puts "This is count #{number}"
+end
+
+# same as above, but in a more Ruby style
+# this and the next one are the preferred
+# way Ruby for-loops are written
+fruits.each do |fruit|
+  puts "A fruit of type: #{fruit}"
+end
+
+# also we can go through mixed lists
+# note this is yet another style, exactly like above
+# but a different syntax (way to write it).
+change.each {|i| puts "I got #{i}" }
+
+# we can also build lists, first start with an empty one
+elements = []
+
+# then use the range operator to do 0 to 5 counts
+(1..6).each do |i|
+  puts "adding #{i} to the list."
+  # pushes the i variable on the *end* of the list
+  elements << i
+end
+
+# now we can print them out too
+elements.each {|i| puts "Elements was: #{i}" }
+
+# Study Drills
+# 1.The range operator works by finding number starting at the first digit then the last between the two dots. (0..5) will start at 0 and then go up to 5, which totals 6 numbers.
+
+# Note: "Why is a for-loop able to use a variable that isn't defined yet?" --> The variable is defined by the `for-loop` when it starts, initializing it to the current element of hte loop iteration each time through.
